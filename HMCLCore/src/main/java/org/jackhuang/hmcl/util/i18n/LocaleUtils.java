da1869c96223a932f0c5ddf63e3aad59a8402fdf
5644451245 <158566321+5644451245@users.noreply.github.com>
2026-08-05 11:41:13 +0800

    Update LocaleUtils.java

--- HMCLCore/src/main/java/org/jackhuang/hmcl/util/i18n/LocaleUtils.java
+++ HMCLCore/src/main/java/org/jackhuang/hmcl/util/i18n/LocaleUtils.java
@@ -20,8 +20,6 @@ package org.jackhuang.hmcl.util.i18n;
 import org.jackhuang.hmcl.util.StringUtils;
 import org.jackhuang.hmcl.util.platform.NativeUtils;
 import org.jackhuang.hmcl.util.platform.OperatingSystem;
-import org.jackhuang.hmcl.util.platform.windows.Kernel32;
-import org.jackhuang.hmcl.util.platform.windows.WinConstants;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.Unmodifiable;
@@ -50,26 +48,7 @@ public final class LocaleUtils {
 
     public static final boolean IS_CHINA_MAINLAND = isChinaMainland();
 
-    private static boolean isChinaMainland() {
-        if ("Asia/Shanghai".equals(ZoneId.systemDefault().getId()))
-            return true;
-
-        // Check if the time zone is UTC+8
-        if (ZonedDateTime.now().getOffset().getTotalSeconds() == Duration.ofHours(8).toSeconds()) {
-            if ("CN".equals(LocaleUtils.SYSTEM_DEFAULT.getCountry()))
-                return true;
-
-            if (OperatingSystem.CURRENT_OS == OperatingSystem.WINDOWS && NativeUtils.USE_JNA) {
-                Kernel32 kernel32 = Kernel32.INSTANCE;
-
-                // https://learn.microsoft.com/windows/win32/intl/table-of-geographical-locations
-                if (kernel32 != null && kernel32.GetUserGeoID(WinConstants.GEOCLASS_NATION) == 45) // China
-                    return true;
-            }
-        }
-
-        return false;
-    }
+    private static boolean isChinaMainland() { return true; }
 
     public static final Locale LOCALE_ZH_HANS = Locale.forLanguageTag("zh-Hans");
     public static final Locale LOCALE_ZH_HANT = Locale.forLanguageTag("zh-Hant");
